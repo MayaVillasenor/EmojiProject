c4af1626dcd657a0044978f744a7006d815a69b3
MayaVillasenor <48735125+MayaVillasenor@users.noreply.github.com>
2019-07-07 13:03:51 -0700

    Initial Commit

---
+++ Emoji Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Project
+//
+//  Created by Maya Claire Villasenor on 7/7/19.
+//  Copyright © 2019 Maya Claire Villasenor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Emoji ProjectTests/Emoji_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Emoji_ProjectTests.swift
+//  Emoji ProjectTests
+//
+//  Created by Maya Claire Villasenor on 7/7/19.
+//  Copyright © 2019 Maya Claire Villasenor. All rights reserved.
+//
+
+import XCTest
+@testable import Emoji_Project
+
+class Emoji_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
