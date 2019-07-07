e1f610b6946a88a12378f6094dd6896c4d89f8a6
MayaVillasenor <48735125+MayaVillasenor@users.noreply.github.com>
2019-07-07 14:49:35 -0700

    First commit

--- Emoji Project/ViewController.swift
+++ Emoji Project/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let emojis = ["😄": "happy face", "😢": "sad face"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Emoji App", message: "Hope everything is ok!" , preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+
+    }
 
 }
 
